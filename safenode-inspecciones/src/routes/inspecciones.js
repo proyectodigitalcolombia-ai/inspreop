3f13de87f97b01514464ad2c3fcf930384e6a7ce
proyectodigitalcolombia-ai <proyectodigitalcolombia@gmail.com>
2026-03-13 08:25:13 -0500

    Update inspecciones.js

--- safenode-inspecciones/src/routes/inspecciones.js
+++ safenode-inspecciones/src/routes/inspecciones.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database');
+const db = require('../database.js');
 
 // Guardar nueva inspección
 router.post('/guardar', async (req, res) => {
